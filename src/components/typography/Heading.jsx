40f311841c6b995ee953f243a497d2011688b87d
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-07-22 13:01:04 +0200

    Update Heading.jsx

--- src/components/typography/Heading.jsx
+++ src/components/typography/Heading.jsx
@@ -1,16 +1,16 @@
-import './heading.css';
+import "./Heading.css";
 
 export const Heading = ({ children, variant }) => {
   const headingVariants = {
-    primary: 'primary',
-    secondary: 'secondary',
-    numbers: 'numbers',
+    primary: "primary",
+    secondary: "secondary",
+    numbers: "numbers",
   };
 
   return (
-    <div className={'heading_container _' + headingVariants[variant]}>
-      <h1 className={'heading _' + headingVariants[variant]}>{children}</h1>
-      <div className={'heading_stroke _' + headingVariants[variant]}>
+    <div className={"heading_container _" + headingVariants[variant]}>
+      <h1 className={"heading _" + headingVariants[variant]}>{children}</h1>
+      <div className={"heading_stroke _" + headingVariants[variant]}>
         {children}
       </div>
     </div>
