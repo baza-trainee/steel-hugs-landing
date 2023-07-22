b78f5cb968c263a60293815f3e72feb97c3e4132
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-07-22 13:17:34 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Brigada from "./components/Brigada/Brigada";
 import Footer from "./components/Footer";
+import { Header } from "./components/UI/Header/Header";
 function App() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   );
