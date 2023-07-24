83c3dda2c008f9c722014d2ffd8674a5919e649a
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-07-24 19:15:55 +0200

    finished goal section + progress bar

--- src/components/UI/Goal/Goal.jsx
+++ src/components/UI/Goal/Goal.jsx
@@ -1,29 +1,35 @@
 import { Heading } from "../../typography/Heading";
 import styles from "./Goal.module.css";
 import DonateButton from "../DonateButton/DonateButton";
+import ProgresBar from "../ProgresBar/ProgresBar";
 export default function Goal({ modal }) {
   return (
     <section>
-      <div className="container">
+      <div className={styles.container}>
         <div className={styles.title}>
           <Heading variant="primary">Сталеві Обійми</Heading>
         </div>
-        <p className={styles.save}>Врятуємо життя тим, хто рятує наше щодня!</p>
+        <div className={styles.flexBox}>
+          <p className={styles.save}>
+            Врятуємо життя тим, хто рятує наше щодня!
+          </p>
 
-        <div className={styles.contentBox}>
-          <div className={styles.image}></div>
-          <div className={styles.textBox}>
-            <p className={styles.text}>Наша мета забезпечити 59 бригаду на</p>
+          <div className={styles.contentBox}>
+            <div className={styles.image}></div>
+            <div className={styles.textBox}>
+              <p className={styles.text}>Наша мета забезпечити 59 бригаду на</p>
 
-            <div className={styles.number}>
-              <Heading variant="numbers">117 042 грн</Heading>
-            </div>
+              <div className={styles.number}>
+                <Heading variant="numbers">117 042 грн</Heading>
+              </div>
 
-            <div className={styles.button}>
-              <DonateButton modal={modal}>Обійняти</DonateButton>
+              <div className={styles.button}>
+                <DonateButton modal={modal}>Обійняти</DonateButton>
+              </div>
             </div>
           </div>
         </div>
+        <ProgresBar />
       </div>
     </section>
   );
