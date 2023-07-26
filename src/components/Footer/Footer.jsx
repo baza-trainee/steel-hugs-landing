ad54a530d4523c2384d5eeeeacc4e932df6879ef
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-07-26 20:59:49 +0200

    fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import styles from './Footer.module.css';
-import ShareButton from '../UI/ShareButton/ShareButton';
+import React from "react";
+import styles from "./Footer.module.css";
+import ShareButton from "../UI/ShareButton/ShareButton";
 
 export default function Footer() {
   return (
-    <section className={styles.footer}>
-      <div className={styles.footerContent}>
-        <div className={styles.footerHead}>
-          <h3>Допоможи іншим дізнатися про наш збір!</h3>
+    <footer>
+      <div className="container">
+        <div className={styles.footerContent}>
+          <div className={styles.footerHead}>
+            <h3>Допоможи іншим дізнатися про наш збір!</h3>
+          </div>
+          <div className={styles.footerButton}>
+            <ShareButton />
+          </div>
         </div>
-        <div className={styles.footerButton}>
-          <ShareButton />
+        <div className={styles.footerRights}>
+          <p>Розробка Baza Trainee Ukraine 2023. Всі права захищені.</p>
         </div>
       </div>
-      <div className={styles.footerRights}>
-        <p>Розробка Baza Trainee Ukraine 2023. Всі права захищені.</p>
-      </div>
-    </section>
+    </footer>
   );
 }
--- src/components/Gathering/Gathering.jsx
+++ src/components/Gathering/Gathering.jsx
@@ -1,29 +1,26 @@
-import React from "react";
-import "./Gathering.css";
 import { Heading } from "../typography/Heading";
-import title from "../../images/gathering.png";
-import bulletproof from "../../images/Group40.png";
+import styles from "./Gathering.module.css";
 
 export default function Gathering() {
   return (
-    <div className="gathering-body">
-      <div className="gathering-text">
-        <div className="gathering-title">
+    <section>
+      <div className="container">
+        <div className={styles.title}>
           <Heading variant="secondary">Збір</Heading>
         </div>
-        <p className="gathering-subtitle">Чому потрібен волонтерський збір?</p>
-      </div>
-      <div className="gathering-bulletproof">
-        <div className="gathering-images">
-          <img src={bulletproof} alt="Бронежелет" />
-        </div>
-        <div className="gathering-bulletproof-items">
-          <p className="gathering-bulletproof-item_first">
-            Нашій бригаді постійно треба відновлювати стан бронежилетів,
-            оскільки вони є життєво важливими засобами захисту для захисників.
-          </p>
-          <div className="gathering-activ">
-            <p className="gathering-bulletproof-item_second">
+        <p className={styles.subtitle}>Чому потрібен волонтерський збір?</p>
+        <p className={styles.firstTextSmall}>
+          Нашій бригаді постійно треба відновлювати стан бронежилетів, оскільки
+          вони є життєво важливими засобами захисту для захисників.
+        </p>
+        <div className={styles.contentBox}>
+          <div className={styles.image}></div>
+          <div className={styles.textBox}>
+            <p className={styles.firstText}>
+              Нашій бригаді постійно треба відновлювати стан бронежилетів,
+              оскільки вони є життєво важливими засобами захисту для захисників.
+            </p>
+            <p className={styles.secondText}>
               Забезпечення наших воїнів високоякісними бронежилетами підвищить
               їх шанси на успішне виконання завдань на передовій та збережуть
               життя, що є найважливішим!
@@ -31,6 +28,6 @@ export default function Gathering() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
