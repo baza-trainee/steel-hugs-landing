6ee322f1d1abcfdbfb1eae1ad162f763e70691fd
Solodiuk Oleksandr <alekswlad85@gmail.com>
2023-07-26 13:06:37 +0300

    Gathering

--- src/components/Gathering/Gathering.jsx
+++ src/components/Gathering/Gathering.jsx
@@ -1,7 +1,9 @@
-
-
 import React from 'react';
+
+
+
 import './Gathering.css';
+import { Heading } from "../typography/Heading";
 import title from '../../images/gathering.png'
 import bulletproof from '../../images/Group40.png'
 
@@ -9,7 +11,7 @@ import bulletproof from '../../images/Group40.png'
   return (
     <div className='gathering-body'>
         <div className='gathering-text'>
-          <div className='gathering-title'><img src={title} alt="Збір"/></div>
+          <div className='gathering-title'><Heading variant="secondary">Збір</Heading></div>
           <p className='gathering-subtitle'>Чому потрібен волонтерський збір?</p>
           </div>
           <div className='gathering-bulletproof'>
@@ -18,7 +20,8 @@ import bulletproof from '../../images/Group40.png'
                 </div>
               <div className='gathering-bulletproof-items'>
             <p  className='gathering-bulletproof-item_first'>Нашій бригаді постійно треба відновлювати стан бронежилетів, оскільки вони є життєво важливими засобами захисту для захисників.</p>
-            <p className='gathering-bulletproof-item_second'>Забезпечення наших воїнів високоякісними бронежилетами підвищить їх шанси на успішне виконання завдань на передовій та збережуть життя, що є найважливішим!</p>
+            <div className='gathering-activ'><p className='gathering-bulletproof-item_second'>Забезпечення наших воїнів високоякісними бронежилетами підвищить їх шанси на успішне виконання завдань на передовій та збережуть життя, що є найважливішим!</p>
+            </div>
             </div>
           </div>
     </div>
