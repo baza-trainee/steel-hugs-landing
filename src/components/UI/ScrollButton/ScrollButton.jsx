9547fdaa05a41511d07e43ad5b9000beb7a66a76
maxkulbako <mkulbako@gmail.com>
2023-07-29 10:35:00 +0200

    outoscroll: + responsive design

--- src/components/UI/ScrollButton/ScrollButton.jsx
+++ src/components/UI/ScrollButton/ScrollButton.jsx
@@ -19,42 +19,6 @@ export const ScrollButton = ({ isVisible }) => {
     }, 500);
   };
 
-  // const handleScroll = () => {
-  //   const button = ref.current;
-
-  //   const windowWidth = window.innerWidth; // Получаем ширину окна
-
-  //   const finalPosition = -windowWidth; // Сдвигаем кнопку на ширину окна влево
-  //   const duration = 1500; // Продолжительность анимации в миллисекундах
-  //   const framesPerSecond = 60; // Число кадров в секунду
-  //   const frameStep = finalPosition / ((duration / 1000) * framesPerSecond); // Шаг смещения на каждом кадре
-
-  //   let currentPosition = 0;
-  //   let currentFrame = 0;
-
-  //   const moveButton = () => {
-  //     currentPosition += frameStep;
-  //     currentFrame++;
-
-  //     if (currentFrame <= (duration / 1000) * framesPerSecond) {
-  //       button.style.transform = `translateX(${currentPosition}px)`;
-  //       requestAnimationFrame(moveButton);
-  //     } else {
-  //       button.style.transform = `translateX(${finalPosition}px)`;
-  //       setIsClicked(false);
-  //     }
-  //   };
-
-  //   requestAnimationFrame(moveButton);
-
-  //   setTimeout(() => {
-  //     window.scrollTo({
-  //       top: 0,
-  //       behavior: "smooth",
-  //     });
-  //   }, 2000);
-  // };
-
   return (
     <div
       ref={ref}
